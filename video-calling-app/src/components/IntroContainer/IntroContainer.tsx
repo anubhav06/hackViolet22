68b0a6d538a945eb79cbfdc38873152e22340de9
Anubhav Gupta <mail.anubhav06@gmail.com>
2022-02-06 19:08:26 +0530

    updated name
    Signed-off-by: Anubhav Gupta <mail.anubhav06@gmail.com>

--- video-calling-app/src/components/IntroContainer/IntroContainer.tsx
+++ video-calling-app/src/components/IntroContainer/IntroContainer.tsx
@@ -108,7 +108,7 @@ const IntroContainer = (props: IntroContainerProps) => {
             <div className={classes.logoContainer}>
               <VideoLogo />
               <Typography variant="h6" className={classes.title}>
-                Meeting by Connect
+                Meetings by SheForum
               </Typography>
             </div>
           </div>
